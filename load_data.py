7a3afb0a971d653601ab274a995726dce7224ca7
Ian Jiang <jiangjs03@gmail.com>
2026-04-06 11:17:24 -0500

    HW3

---
+++ load_data.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from pymongo import MongoClient
+
+MONGO_URI = "mongodb+srv://lab6user:030302@cluster0.s42bhpa.mongodb.net/?appName=Cluster0"
+DB_NAME = "ev_db"      # 
+COLLECTION_NAME = "vehicles" # 
+
+def main():
+    client = MongoClient(MONGO_URI)
+    db = client[DB_NAME]
+    collection = db[COLLECTION_NAME]
+
+    print("Reading .CSV File...")
+    df = pd.read_csv('ev_data.csv')
+    
+    data = df.to_dict('records')
+
+    batch_size = 1000
+    print(f"Current Bacth {batch_size} speed import {len(data)} data..")
+    
+    for i in range(0, len(data), batch_size):
+        batch = data[i : i + batch_size]
+        collection.insert_many(batch)
+    
+    print("Sucess！")
+
+if __name__ == "__main__":
+    main()
